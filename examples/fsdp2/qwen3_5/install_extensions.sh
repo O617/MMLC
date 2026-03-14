6c38018f09a70f99662f0b62b4a53b4d76b134a5
compasszzn <1065007960@qq.com>
2026-03-14 20:48:27 +0800

    RM unrelated files

--- examples/fsdp2/qwen3_5/install_extensions.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/huggingface/transformers.git
-cd transformers
-git checkout fc91372
-pip install -e .
-cd ..
-pip install triton-ascend==3.2.0
-pip install accelerate==1.2.0
